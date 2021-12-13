bed0be50f57ddc164f8675081bd0830964cda706
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-13 14:29:04 +0300

    creates project model

--- award/models.py
+++ award/models.py
@@ -24,3 +24,43 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    cat = models.TextField(max_length=20)
+    location = models.TextField(max_length=20)
+    image = CloudinaryField('image')
+    url = models.URLField(max_length=60, null=True)
+    about = models.TextField(max_length=2000)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+
+    def _str_(self):
+        return self.name
+
+    @classmethod
+    def get_all_projects(cls):
+        projects = Project.objects.all()
+        return projects
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        projects = cls.objects.filter(name__icontains=search_term)
+        return projects
+
+    @classmethod
+    def display_all_projects(cls):
+        return cls.objects.all()
+
+    def save_project(self):
+        self.save()
+
+    def update_project(self, name, description, category):
+        self.name = name,
+        self.description = description,
+        self.category = category
+        self.save()
+
+    class Meta:
+        ordering = ['-created_on']
