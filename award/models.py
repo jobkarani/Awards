18e38c1f62aa4d0d99a5514c329cc3934162daa2
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-13 14:28:07 +0300

    creates profile model

--- award/models.py
+++ award/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+import datetime as dt
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=650, blank=True, null=True)
+    contact = models.CharField(max_length=20, blank=True, null=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.save()
+
+    def update(self):
+        self.save()
+
+    def __str__(self):
+        return self.user.username
