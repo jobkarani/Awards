1a346485c4ab49d3f45c0b0a81ac8542dbee77ef
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-15 08:58:38 +0300

    deployment to heroku

--- awardss/settings.py
+++ awardss/settings.py
@@ -107,10 +107,13 @@ WSGI_APPLICATION = 'awardss.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+    'PASSWORD':config("DB_PASSWORD"),
     }
 }
+
 
 
 # Password validation
